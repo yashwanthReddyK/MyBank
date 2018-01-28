134c23d7e3ae9c91f6ebc404875449959574e704
yashwanthReddyK <yashwanthreddy.kumbham@gmail.com>
2018-01-28 14:08:50 -0500

    cerated java files

---
+++ src/main/java/com/Java/UseraRegisterDetails.java
@@ -0,0 +1,75 @@
+package com.Java;
+
+public class UseraRegisterDetails {
+
+    String name;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String password;
+    String confirmPassword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+
+
+
+
+
+
+}
